9a27bc8528169235990cd0a27fd1750b43c84f12
PedroPereiraJPPF <joaopedropereira201@gmail.com>
2025-05-08 14:37:43 -0300

    up

--- src/main/java/com/arboviroses/conectaDengue/Domain/Filters/NotificationFilters.java
+++ src/main/java/com/arboviroses/conectaDengue/Domain/Filters/NotificationFilters.java
@@ -2,6 +2,7 @@ package com.arboviroses.conectaDengue.Domain.Filters;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import com.arboviroses.conectaDengue.Api.DTO.response.AgravoCountBySemanaEpidemiologica;
 import com.arboviroses.conectaDengue.Api.DTO.response.BairroCountDTO;
@@ -17,37 +18,16 @@ public class NotificationFilters {
     {
         Integer date = request.getParameter("year") != null ? Integer.valueOf(request.getParameter("year")) : null;
         String agravoName = request.getParameter("agravo");
+        String bairro = request.getParameter("bairro");
         String agravoId = null;
 
         if (agravoName != null) {
             agravoId = ConvertNameToIdAgravo.convert(agravoName);
         }
 
-        if (date != null) {
-            if (agravoId != null) {
-                return new CountAgravoBySexoDTO(
-                    notificationRepository.countByIdAgravoAndSexoAndYearNotification(agravoId, "M", date),
-                    notificationRepository.countByIdAgravoAndSexoAndYearNotification(agravoId, "F", date)
-                );
-            } else {
-                return new CountAgravoBySexoDTO(
-                    notificationRepository.countBySexoAndYearNotification("M", date),
-                    notificationRepository.countBySexoAndYearNotification("F", date)
-                );
-            }
-
-        }   
-
-        if (agravoId != null) {  
-            return new CountAgravoBySexoDTO(
-                notificationRepository.countByIdAgravoAndSexo(agravoId, "M"),
-                notificationRepository.countByIdAgravoAndSexo(agravoId, "F")
-            );
-        }
-
         return new CountAgravoBySexoDTO(
-            notificationRepository.countBySexo("M"),
-            notificationRepository.countBySexo("F")
+            notificationRepository.countByOptionalParams(agravoId, "M", bairro, date, null),
+            notificationRepository.countByOptionalParams(agravoId, "F", bairro, date, null)
         );
     }
 
@@ -55,6 +35,7 @@ public class NotificationFilters {
     {
         Integer date = request.getParameter("year") != null ? Integer.valueOf(request.getParameter("year")) : null;
         String agravoName = request.getParameter("agravo");
+        String bairro = request.getParameter("bairro");
         String agravoId = null;
 
         if (agravoName != null) {
@@ -63,11 +44,21 @@ public class NotificationFilters {
 
         if (date != null) {
             if (agravoId != null) {
+                if (bairro != null) {
+                    return notificationRepository.listarContagemPorSemanaEpidemiologicaByAgravoAndYearAndBairro(agravoId, date, bairro);
+                }
                 return notificationRepository.listarContagemPorSemanaEpidemiologica(agravoId, date);
             }
 
+            if (bairro != null) {
+                return notificationRepository.listarContagemPorSemanaEpidemiologicaByYearAndBairro(date, bairro);
+            }
             return notificationRepository.listarContagemPorSemanaEpidemiologica(date);
         } 
+
+        if (bairro != null) {
+            return notificationRepository.listarContagemPorSemanaEpidemiologicaByBairro(bairro);
+        }
 
         return notificationRepository.listarContagemPorSemanaEpidemiologica();
     }
@@ -75,21 +66,14 @@ public class NotificationFilters {
     public static Map<String, Integer> filtersForNotificationsByAgeRange(HttpServletRequest request, NotificationRepository notificationRepository) throws InvalidAgravoException {
         Integer year = request.getParameter("year") != null ? Integer.valueOf(request.getParameter("year")) : null;
         String agravoName = request.getParameter("agravo");
+        String bairro = request.getParameter("bairro");
         String agravoId = null;
 
         if (agravoName != null) {
             agravoId = ConvertNameToIdAgravo.convert(agravoName);
         }
 
-        if (year != null) {
-            if (agravoId != null) {
-                return notificationRepository.listarContagemPorFaixaDeIdadeByIdAgravo(agravoId, year);
-            }
-
-            return notificationRepository.listarContagemPorFaixaDeIdade(year);
-        }
-
-        return notificationRepository.listarContagemPorFaixaDeIdade();
+        return notificationRepository.listarContagemPorFaixaDeIdadeComFiltros(agravoId, year, bairro);
     }
 
     public static List<BairroCountDTO> filtersForNotificationsCountNeighborhoods(HttpServletRequest request, NotificationRepository notificationRepository) throws InvalidAgravoException {
@@ -115,69 +99,50 @@ public class NotificationFilters {
     public static long filterForCountByIdAgravo(HttpServletRequest request, NotificationRepository notificationRepository) throws Exception {
         Integer year = request.getParameter("year") != null ? Integer.valueOf(request.getParameter("year")) : null;
         String agravoName = request.getParameter("agravo");
-        String agravoId = null;
+        String bairro = request.getParameter("bairro");
 
-        if (agravoName == null) {
-            if (year == null) {
-                return notificationRepository.count();
-            } 
+        String agravoId = ConvertNameToIdAgravo.convert(agravoName);
 
-            return notificationRepository.countByYearNotification(year);
-        }
-
-        agravoId = ConvertNameToIdAgravo.convert(agravoName);
-
-        if (year != null) {
-            return notificationRepository.countByIdAgravoAndYearNotification(agravoId, year);
-        }
-
-        return notificationRepository.countByIdAgravo(agravoId);
+        return notificationRepository.countByOptionalParams(agravoId, null, bairro, year, null);
     }
 
     public static long filterForCountByEvolucao(HttpServletRequest request, NotificationRepository notificationRepository) throws Exception {
         Integer year = request.getParameter("year") != null ? Integer.valueOf(request.getParameter("year")) : null;
         String agravoName = request.getParameter("agravo");
-        String agravoId = null;
-        String evolucao;
+        String bairro = request.getParameter("bairro");
+        String agravoId = ConvertNameToIdAgravo.convert(agravoName);
+        String evolucao = Optional.ofNullable(request.getParameter("evolucao"))
+            .orElseThrow(() -> new Exception("Informe o nivel da evolucao"));
 
-        if (request.getParameter("evolucao") == null) {
-            throw new Exception("Informe o nivel da evolucao");
-        }
-
-        evolucao = request.getParameter("evolucao");
-
-        if (agravoName == null) {
-            if (year == null) {
-                return notificationRepository.countByEvolucao(evolucao);
-            } 
-
-            return notificationRepository.countByEvolucaoAndYear(evolucao, year);
-        }
-
-        agravoId = ConvertNameToIdAgravo.convert(agravoName);
-
-        if (year != null) {
-            return notificationRepository.countByIdAgravoAndEvolucaoAndYear(agravoId, evolucao, year);
-        }
-
-        return notificationRepository.countByIdAgravoAndEvolucao(agravoId, evolucao);
+        return notificationRepository.countByOptionalParams(agravoId, agravoId, bairro, year, evolucao);
     } 
 
     public static Map<String, Integer> filterForListByDadosEspecificados(HttpServletRequest request, NotificationRepository notificationRepository) throws Exception {
         Integer year = request.getParameter("year") != null ? Integer.valueOf(request.getParameter("year")) : null;
         String agravoName = request.getParameter("agravo");
+        String bairro = request.getParameter("bairro");
         String agravoId = null;
 
         if (agravoName == null) {
             if (year == null) {
-                return notificationRepository.listarDadosEspecificadosDasNotificacoes();
+                if (bairro == null) {
+                    return notificationRepository.listarDadosEspecificadosDasNotificacoes();
+                }
+                return notificationRepository.listarDadosEspecificadosDasNotificacoesByBairro(bairro);
             } 
         }
 
         agravoId = ConvertNameToIdAgravo.convert(agravoName);
 
         if (year != null) {
+            if (bairro != null) {
+                return notificationRepository.listarDadosEspecificadosDasNotificacoesByIdAgravoAndYearAndBairro(agravoId, year, bairro);
+            }
             return notificationRepository.listarDadosEspecificadosDasNotificacoes(agravoId, year);
+        }
+
+        if (bairro != null) {
+            return notificationRepository.listarDadosEspecificadosDasNotificacoesByIdAgravoAndBairro(agravoId, bairro);
         }
 
         return notificationRepository.listarDadosEspecificadosDasNotificacoes(agravoId);
--- src/main/java/com/arboviroses/conectaDengue/Domain/Repositories/Notifications/NotificationRepository.java
+++ src/main/java/com/arboviroses/conectaDengue/Domain/Repositories/Notifications/NotificationRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.arboviroses.conectaDengue.Api.DTO.response.AgravoCountBySemanaEpidemiologica;
@@ -26,6 +27,23 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     long countByIdAgravoAndSexo(String idAgravo, String sexo);
 
     long countByIdAgravoAndDataNotification(String idAgravo, Date date);
+
+    @Query("""
+        SELECT COUNT(n) 
+        FROM Notification as n 
+        WHERE (:agravoId IS NULL OR n.idAgravo = :agravoId)
+        AND (:sexo IS NULL OR n.sexo = :sexo)
+        AND (:nomeBairro IS NULL OR n.nomeBairro = :nomeBairro)
+        AND (:year IS NULL OR YEAR(n.dataNotification) = :year)
+        AND (:evolucao IS NULL OR n.evolucao = :evolucao)
+    """)
+    long countByOptionalParams(
+        @Param("agravoId") String agravoId,
+        @Param("sexo") String sexo,
+        @Param("nomeBairro") String nomeBairro,
+        @Param("year") Integer year,
+        @Param("evolucao") String evolucao
+    );
 
     @Query("""
         SELECT count(n.idNotification) FROM Notification as n where n.idAgravo = :agravo and YEAR(n.dataNotification) = :year 
@@ -148,52 +166,31 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 
     @Query("""
         SELECT 
-            SUM(CASE WHEN n.idadePaciente BETWEEN 1 AND 10 THEN 1 ELSE 0 END) AS age1to10,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 11 AND 20 THEN 1 ELSE 0 END) AS age11to20,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 21 AND 30 THEN 1 ELSE 0 END) AS age21to30,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 31 AND 40 THEN 1 ELSE 0 END) AS age31to40,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 41 AND 50 THEN 1 ELSE 0 END) AS age41to50,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 51 AND 60 THEN 1 ELSE 0 END) AS age51to60,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 61 AND 70 THEN 1 ELSE 0 END) AS age61to70,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 71 AND 80 THEN 1 ELSE 0 END) AS age71to80,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 81 AND 90 THEN 1 ELSE 0 END) AS age81to90,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 91 AND 99 THEN 1 ELSE 0 END) AS age91to99
-        FROM Notification as n
+            SUM(CASE WHEN n.idadePaciente BETWEEN 0 AND 1 THEN 1 ELSE 0 END) AS age0to1,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 2 AND 3 THEN 1 ELSE 0 END) AS age2to3,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 4 AND 5 THEN 1 ELSE 0 END) AS age4to5,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 6 AND 7 THEN 1 ELSE 0 END) AS age6to7,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 8 AND 9 THEN 1 ELSE 0 END) AS age8to9,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 10 AND 19 THEN 1 ELSE 0 END) AS age10to19,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 20 AND 29 THEN 1 ELSE 0 END) AS age20to29,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 30 AND 39 THEN 1 ELSE 0 END) AS age30to39,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 40 AND 49 THEN 1 ELSE 0 END) AS age40to49,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 50 AND 59 THEN 1 ELSE 0 END) AS age50to59,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 60 AND 69 THEN 1 ELSE 0 END) AS age60to69,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 70 AND 79 THEN 1 ELSE 0 END) AS age70to79,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 80 AND 89 THEN 1 ELSE 0 END) AS age80to89,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 90 AND 99 THEN 1 ELSE 0 END) AS age90to99
+        FROM Notification n
+        WHERE (:year IS NULL OR FUNCTION('YEAR', n.dataNotification) = :year)
+          AND (:idAgravo IS NULL OR n.idAgravo = :idAgravo)
+          AND (:nomeBairro IS NULL Or n.nomeBairro = :nomeBairro)
     """)
-    Map<String, Integer> listarContagemPorFaixaDeIdade();
-
-    @Query("""
-        SELECT 
-            SUM(CASE WHEN n.idadePaciente BETWEEN 1 AND 10 THEN 1 ELSE 0 END) AS age1to10,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 11 AND 20 THEN 1 ELSE 0 END) AS age11to20,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 21 AND 30 THEN 1 ELSE 0 END) AS age21to30,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 31 AND 40 THEN 1 ELSE 0 END) AS age31to40,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 41 AND 50 THEN 1 ELSE 0 END) AS age41to50,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 51 AND 60 THEN 1 ELSE 0 END) AS age51to60,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 61 AND 70 THEN 1 ELSE 0 END) AS age61to70,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 71 AND 80 THEN 1 ELSE 0 END) AS age71to80,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 81 AND 90 THEN 1 ELSE 0 END) AS age81to90,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 91 AND 99 THEN 1 ELSE 0 END) AS age91to99
-        FROM Notification as n where YEAR(n.dataNotification) = :year
-    """)
-    Map<String, Integer> listarContagemPorFaixaDeIdade(int year);
-
-    @Query("""
-        SELECT 
-            SUM(CASE WHEN n.idadePaciente BETWEEN 1 AND 10 THEN 1 ELSE 0 END) AS age1to10,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 11 AND 20 THEN 1 ELSE 0 END) AS age11to20,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 21 AND 30 THEN 1 ELSE 0 END) AS age21to30,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 31 AND 40 THEN 1 ELSE 0 END) AS age31to40,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 41 AND 50 THEN 1 ELSE 0 END) AS age41to50,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 51 AND 60 THEN 1 ELSE 0 END) AS age51to60,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 61 AND 70 THEN 1 ELSE 0 END) AS age61to70,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 71 AND 80 THEN 1 ELSE 0 END) AS age71to80,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 81 AND 90 THEN 1 ELSE 0 END) AS age81to90,
-            SUM(CASE WHEN n.idadePaciente BETWEEN 91 AND 99 THEN 1 ELSE 0 END) AS age91to99
-        FROM Notification as n where YEAR(n.dataNotification) = :year and idAgravo = :idAgravo
-    """)
-    Map<String, Integer> listarContagemPorFaixaDeIdadeByIdAgravo(String idAgravo, int year);
-
+    Map<String, Integer> listarContagemPorFaixaDeIdadeComFiltros(
+        @Param("idAgravo") String idAgravo,
+        @Param("year") Integer year,
+        @Param("nomeBairro") String nomeBairro
+    );
+    
     @Query("""
         SELECT 
             sum(case when n.classificacao = '1' then 1 else 0 end) AS confirmados,
@@ -229,4 +226,157 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
         FROM Notification AS n where n.idAgravo = :idAgravo and YEAR(n.dataNotification) = :year
     """)
     Map<String, Integer> listarDadosEspecificadosDasNotificacoes(String idAgravo, int year);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.evolucao = :evolucao and n.nomeBairro = :bairro
+        """)
+    long countByEvolucaoAndBairro(String evolucao, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.evolucao = :evolucao and YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+        """)
+    long countByEvolucaoAndYearAndBairro(String evolucao, int year, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.idAgravo = :idAgravo and n.evolucao = :evolucao and n.nomeBairro = :bairro
+        """)
+    long countByIdAgravoAndEvolucaoAndBairro(String idAgravo, String evolucao, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.idAgravo = :idAgravo and n.evolucao = :evolucao and YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+        """)
+    long countByIdAgravoAndEvolucaoAndYearAndBairro(String idAgravo, String evolucao, long year, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+        """)
+    long countByYearAndBairro(long year, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.sexo = :sexo and n.nomeBairro = :bairro
+        """)
+    long countBySexoAndBairro(String sexo, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.idAgravo = :idAgravo and n.sexo = :sexo and n.nomeBairro = :bairro
+        """)
+    long countByIdAgravoAndSexoAndBairro(String idAgravo, String sexo, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.idAgravo = :idAgravo and n.sexo = :sexo and YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+        """)
+    long countByIdAgravoAndSexoAndYearAndBairro(String idAgravo, String sexo, long year, String bairro);
+
+    @Query("""
+        SELECT count(n.idNotification) FROM Notification as n where n.sexo = :sexo and YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+        """)
+    long countBySexoAndYearAndBairro(String sexo, long year, String bairro);
+
+    @Query("""
+        SELECT new com.arboviroses.conectaDengue.Api.DTO.response.AgravoCountBySemanaEpidemiologica(n.semanaEpidemiologica, count(n.idNotification), n.idAgravo) 
+        from Notification as n where n.nomeBairro = :bairro
+        group by n.semanaEpidemiologica, n.idAgravo
+        ORDER BY n.semanaEpidemiologica
+        """)
+    List<AgravoCountBySemanaEpidemiologica> listarContagemPorSemanaEpidemiologicaByBairro(String bairro);
+
+    @Query("""
+        SELECT new com.arboviroses.conectaDengue.Api.DTO.response.AgravoCountBySemanaEpidemiologica(n.semanaEpidemiologica, count(n.idNotification), n.idAgravo) 
+        from Notification as n where YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+        group by n.semanaEpidemiologica, n.idAgravo
+        ORDER BY n.semanaEpidemiologica
+        """)
+    List<AgravoCountBySemanaEpidemiologica> listarContagemPorSemanaEpidemiologicaByYearAndBairro(int year, String bairro);
+
+    @Query("""
+        SELECT new com.arboviroses.conectaDengue.Api.DTO.response.AgravoCountBySemanaEpidemiologica(n.semanaEpidemiologica, count(n.idNotification), n.idAgravo) 
+        from Notification as n where YEAR(n.dataNotification) = :year and n.idAgravo = :agravo and n.nomeBairro = :bairro
+        group by n.semanaEpidemiologica, n.idAgravo
+        ORDER BY n.semanaEpidemiologica
+        """)
+    List<AgravoCountBySemanaEpidemiologica> listarContagemPorSemanaEpidemiologicaByAgravoAndYearAndBairro(String agravo, int year, String bairro);
+
+    @Query("""
+        SELECT 
+            SUM(CASE WHEN n.idadePaciente BETWEEN 1 AND 10 THEN 1 ELSE 0 END) AS age1to10,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 11 AND 20 THEN 1 ELSE 0 END) AS age11to20,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 21 AND 30 THEN 1 ELSE 0 END) AS age21to30,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 31 AND 40 THEN 1 ELSE 0 END) AS age31to40,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 41 AND 50 THEN 1 ELSE 0 END) AS age41to50,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 51 AND 60 THEN 1 ELSE 0 END) AS age51to60,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 61 AND 70 THEN 1 ELSE 0 END) AS age61to70,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 71 AND 80 THEN 1 ELSE 0 END) AS age71to80,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 81 AND 90 THEN 1 ELSE 0 END) AS age81to90,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 91 AND 99 THEN 1 ELSE 0 END) AS age91to99
+        FROM Notification as n where n.nomeBairro = :bairro
+    """)
+    Map<String, Integer> listarContagemPorFaixaDeIdadeByBairro(String bairro);
+
+    @Query("""
+        SELECT 
+            SUM(CASE WHEN n.idadePaciente BETWEEN 1 AND 10 THEN 1 ELSE 0 END) AS age1to10,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 11 AND 20 THEN 1 ELSE 0 END) AS age11to20,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 21 AND 30 THEN 1 ELSE 0 END) AS age21to30,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 31 AND 40 THEN 1 ELSE 0 END) AS age31to40,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 41 AND 50 THEN 1 ELSE 0 END) AS age41to50,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 51 AND 60 THEN 1 ELSE 0 END) AS age51to60,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 61 AND 70 THEN 1 ELSE 0 END) AS age61to70,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 71 AND 80 THEN 1 ELSE 0 END) AS age71to80,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 81 AND 90 THEN 1 ELSE 0 END) AS age81to90,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 91 AND 99 THEN 1 ELSE 0 END) AS age91to99
+        FROM Notification as n where YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+    """)
+    Map<String, Integer> listarContagemPorFaixaDeIdadeByYearAndBairro(int year, String bairro);
+
+    @Query("""
+        SELECT 
+            SUM(CASE WHEN n.idadePaciente BETWEEN 1 AND 10 THEN 1 ELSE 0 END) AS age1to10,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 11 AND 20 THEN 1 ELSE 0 END) AS age11to20,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 21 AND 30 THEN 1 ELSE 0 END) AS age21to30,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 31 AND 40 THEN 1 ELSE 0 END) AS age31to40,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 41 AND 50 THEN 1 ELSE 0 END) AS age41to50,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 51 AND 60 THEN 1 ELSE 0 END) AS age51to60,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 61 AND 70 THEN 1 ELSE 0 END) AS age61to70,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 71 AND 80 THEN 1 ELSE 0 END) AS age71to80,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 81 AND 90 THEN 1 ELSE 0 END) AS age81to90,
+            SUM(CASE WHEN n.idadePaciente BETWEEN 91 AND 99 THEN 1 ELSE 0 END) AS age91to99
+        FROM Notification as n where YEAR(n.dataNotification) = :year and n.idAgravo = :idAgravo and n.nomeBairro = :bairro
+    """)
+    Map<String, Integer> listarContagemPorFaixaDeIdadeByIdAgravoAndYearAndBairro(String idAgravo, int year, String bairro);
+
+    @Query("""
+        SELECT 
+            sum(case when n.classificacao = '1' then 1 else 0 end) AS confirmados,
+            sum(case when n.classificacao = '2' then 1 else 0 end) AS descartados,
+            sum(case when n.evolucao = '1' then 1 else 0 end) AS curados,
+            sum(case when n.evolucao = '2' then 1 else 0 end) AS morte_por_agravo,
+            sum(case when n.evolucao = '3' then 1 else 0 end) AS morte_outras_causas,
+            sum(case when n.evolucao = '4' then 1 else 0 end) AS ignorados
+        FROM Notification AS n where n.nomeBairro = :bairro
+    """)
+    Map<String, Integer> listarDadosEspecificadosDasNotificacoesByBairro(String bairro);
+
+    @Query("""
+        SELECT 
+            sum(case when n.classificacao = '1' then 1 else 0 end) AS confirmados,
+            sum(case when n.classificacao = '2' then 1 else 0 end) AS descartados,
+            sum(case when n.evolucao = '1' then 1 else 0 end) AS curados,
+            sum(case when n.evolucao = '2' then 1 else 0 end) AS morte_por_agravo,
+            sum(case when n.evolucao = '3' then 1 else 0 end) AS morte_outras_causas,
+            sum(case when n.evolucao = '4' then 1 else 0 end) AS ignorados
+        FROM Notification AS n where n.idAgravo = :idAgravo and n.nomeBairro = :bairro
+    """)
+    Map<String, Integer> listarDadosEspecificadosDasNotificacoesByIdAgravoAndBairro(String idAgravo, String bairro);
+
+    @Query("""
+        SELECT 
+            sum(case when n.classificacao = '1' then 1 else 0 end) AS confirmados,
+            sum(case when n.classificacao = '2' then 1 else 0 end) AS descartados,
+            sum(case when n.evolucao = '1' then 1 else 0 end) AS curados,
+            sum(case when n.evolucao = '2' then 1 else 0 end) AS morte_por_agravo,
+            sum(case when n.evolucao = '3' then 1 else 0 end) AS morte_outras_causas,
+            sum(case when n.evolucao = '4' then 1 else 0 end) AS ignorados
+        FROM Notification AS n where n.idAgravo = :idAgravo and YEAR(n.dataNotification) = :year and n.nomeBairro = :bairro
+    """)
+    Map<String, Integer> listarDadosEspecificadosDasNotificacoesByIdAgravoAndYearAndBairro(String idAgravo, int year, String bairro);
 }
