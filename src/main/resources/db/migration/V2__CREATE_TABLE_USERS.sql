c66ef5533696220029a8dab673ee68b535c271cc
PedroPereiraJPPF <joaopedropereira201@gmail.com>
2025-06-23 09:10:20 -0300

    Atualiza o projeto para usar postgresql

--- src/main/resources/db/migration/V2__CREATE_TABLE_USERS.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-    id BIGINT PRIMARY KEY,
-    cpf varchar(15) unique,
-    password varchar(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
